d843b1c203049c1559b515022e3ed543c5ff9336
Julvensly Destine <destinejuvensly@gmail.com>
2026-07-21 23:23:12 -0400

    Premye vèsyon Julex

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+         function App() { 
+return ( 
+<div>
+ <img src="/logo-julex2.png"  alt="Logo Julex2"  className="logo" />
+<h1>BIENVENU CHEZ JULEX</h1>
+<h2>PATE LAKAY</h2> 
+ <div className="product">   
+   <img
+        src="/pate.jpg" 
+        alt="Pate" 
+        width="250"
+  /> 
+      <p>Pri: 100 HTG</p> <button>Achete</button>          
+   </div>
+
+<div className="product">    
+   <img
+     src="/pate2.jpg"
+     alt="pate2"
+     width="250"
+/>
+    <p>pri: 100 HTG</p>
+   <button>Achete</button>
+     </div>
+   </div>
+ )
+}
+export default App
